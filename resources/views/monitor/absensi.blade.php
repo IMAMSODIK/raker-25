13400c3bd2c393905e1dea44cbe9d14176ed95fd
IMAM SODIK <imamsodik00@gmail.com>
2025-11-26 13:08:53 +0700

    adsf

--- resources/views/monitor/absensi.blade.php
+++ resources/views/monitor/absensi.blade.php
@@ -59,7 +59,7 @@
                 <div class="card shadow-sm text-center">
                     <div class="card-body">
                         <h6 class="text-muted">Total Peserta</h6>
-                        <h4 id="totalPeserta">0</h4>
+                        <h4 id="totalPeserta">96</h4>
                     </div>
                 </div>
             </div>
@@ -68,7 +68,7 @@
                 <div class="card shadow-sm text-center">
                     <div class="card-body">
                         <h6 class="text-muted">Total Absensi 2</h6>
-                        <h4 id="totalAbsensi">0</h4>
+                        <h4 id="totalPeserta">0</h4>
                     </div>
                 </div>
             </div>
