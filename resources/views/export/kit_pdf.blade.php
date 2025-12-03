37bece6efd68f8101471a680962d8547835a6051
IMAM SODIK <imamsodik00@gmail.com>
2025-12-04 03:53:50 +0700

    adsf

--- resources/views/export/kit_pdf.blade.php
+++ resources/views/export/kit_pdf.blade.php
@@ -40,7 +40,7 @@
 
                     <td>
                         @if($p->foto)
-                            <img src="{{ asset('storage/uploads/foto_peserta' . '/' . $p->foto) }}" class="foto">
+                            <img src="{{ asset('storage' . '/' . $p->foto) }}" class="foto">
                         @else
                             -
                         @endif
